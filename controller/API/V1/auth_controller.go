d607b06da5bc3ed61a61bae11cc1cdd1ad01fc0e
unknown <supachai.pho@outlook.com>
2023-03-16 16:56:23 +0700

    Update auth_controller.go
    - Logout function

--- controller/API/V1/auth_controller.go
+++ controller/API/V1/auth_controller.go
@@ -7,7 +7,7 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-func Auth(c echo.Context) error {
+func Login(c echo.Context) error {
 	// Bind data from request
 	user := new(models.Login)
 	if err := c.Bind(user); err != nil {
@@ -15,7 +15,7 @@ func Auth(c echo.Context) error {
 	}
 
 	// Call keycloak service API
-	res_data, status, err := keycloak.Auth_Keycloak(user)
+	res_data, status, err := keycloak.LoginKeycloak(user)
 	if err != nil {
 		if err.Error() == "Error" { // Check if API return error
 			return c.JSON(status, map[string]string{"error": res_data["error"].(string)})
@@ -40,4 +40,19 @@ func Auth(c echo.Context) error {
 		"user_id": user_data.ID,
 		"name":    user_data.Name,
 	})
+}
+
+func Logout(c echo.Context) error {
+	// Call keycloak service API
+	res_data, status, err := keycloak.LogoutKeycloak()
+	if err != nil {
+		if err.Error() == "Error" { // Check if API return error
+			return c.JSON(status, map[string]string{"error": res_data["error"].(string)})
+		} else {
+			return c.JSON(status, map[string]string{"error": err.Error()})
+		}
+	}
+
+	// Return response from keycloak service API
+	return c.JSON(status, res_data)
 }
